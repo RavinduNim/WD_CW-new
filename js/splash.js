971301edfec09c1854e50205956af81b2027a4cb
RavinduNim <ravindu.20244067@iit.ac.lk>
2026-03-28 15:47:20 +0530

    Implement Splash Screen

---
+++ js/splash.js
@@ -0,0 +1,13 @@
+let secondsLeft = 4;                                                        
+const countdownElement = document.getElementById("countdown-text");
+
+const countdownInterval = setInterval(() => {
+  secondsLeft--;
+  if (secondsLeft >= 0) {
+    countdownElement.textContent = `Entering site in ${secondsLeft}...`;
+  }
+
+  if (secondsLeft <= 0) {
+    clearInterval(countdownInterval);
+  }
+}, 1000);
