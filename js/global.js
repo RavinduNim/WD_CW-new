6a69f899c3c88ee85c92ecaa3ecc84a95e400845
Dush44 <dushan.20244057@iit.ac.lk>
2026-03-28 18:09:06 +0530

    change back totop function

--- js/global.js
+++ js/global.js
@@ -7,3 +7,27 @@ if (toggle && navLinks) {
     navLinks.classList.toggle("show");
   });
 }
+//funtion of back to Top button
+const reveals = document.querySelectorAll(".reveal");
+const topButton = document.querySelector(".top");
+
+function revealOnScroll() {
+  reveals.forEach((el) => {
+    const windowHeight = window.innerHeight;
+    const elementTop = el.getBoundingClientRect().top;
+
+    if (elementTop < windowHeight - 100) {
+      el.classList.add("active");
+    }
+  });
+
+  // Show go to top button when scrolled down
+  if (window.scrollY > 300) {
+    topButton.classList.add("show");
+  } else {
+    topButton.classList.remove("show");
+  }
+}
+
+window.addEventListener("scroll", revealOnScroll);
+window.addEventListener("load", revealOnScroll);
